04e57a4ea65bb5fcf116e3eaa5f5009b85ad06c6
MSAditama26 <56715870+MSAditama26@users.noreply.github.com>
2020-05-07 11:45:04 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "unknown", quotes: "Whatever happens, keep breathing" },
+    { author: "unknown", quotes: "I love you." },
+    { author: "unknown", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "unknown", quotes: "I love you." },
+    { author: "unknown", quotes: "I'm sorry for loving you." },
 ];
